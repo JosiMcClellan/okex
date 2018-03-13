fe37a51f86f52b21a6850efd1bd96f9741494644
Josi <junipertmcclellan@gmail.com>
2018-03-12 21:21:50 -0600

    extract CommunityIndex::Preview

---
+++ client/src/App/Public/Community/Preview.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
+import { GridListTile, GridListTileBar } from 'material-ui/GridList';
+import { shape, communityShape } from '../propShapes';
+
+const Preview = (community) => {
+  const {
+    slug, name, image, description,
+  } = community;
+  return (
+    <LinkContainer to={{ state: community, pathname: `c/${slug}` }}>
+      <GridListTile>
+        <img alt={name} src={image} />
+        <GridListTileBar
+          title={name}
+          subtitle={description}
+        />
+      </GridListTile>
+    </LinkContainer>
+  );
+};
+Preview.propTypes = {
+  community: shape(communityShape).isRequired,
+};
+
+export default Preview;
