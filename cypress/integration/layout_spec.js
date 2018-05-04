377de97301abac0b121c0785e3bca0610f3181ca
Josi <junipertmcclellan@gmail.com>
2018-05-04 16:27:29 -0500

    debug travis + cypress

--- cypress/integration/layout_spec.js
+++ cypress/integration/layout_spec.js
@@ -1,45 +1,40 @@
-describe('Layout', function() {
-  describe('when I view any page', function() {
-    context('as a visitor', function() {
-      it(`
-        I see a header with:
-          the brand, which is a link to the homepage.
-          a link to the community index.
-          a login button.
-      `, function() {
-        cy.visit('/');
-        cy.get('header a.brand[href="/"]');
-        cy.get('header a[href="/c"]');
-        cy.get('header img[alt="Sign In with Google"]')
-      });
-      it(`
-        I see a footer with:
-          site copyright info.
-          links to public info pages, including
-            site terms.
-            about the site.
-      `, function() {
-        cy.visit('/');
-        cy.get('footer a[href="/terms"]');
-        cy.get('footer a[href="/about"]');
-      });
+describe('When I view any page', function() {
+  context('as a visitor', function() {
+    it(`
+      I see a header with:
+        the brand, which is a link to the homepage.
+        a link to the community index.
+        a login button.
+      I see a footer with:
+        site copyright info.
+        links to public info pages, including
+          site terms.
+          about the site.
+    `, function() {
+      cy.visit('/')
+      cy.get('header a.brand[href="/"]');
+      cy.get('header a[href="/c"]');
+      cy.get('header img[alt="Sign In with Google"]')
+      cy.get('footer a[href="/terms"]');
+      cy.get('footer a[href="/about"]');
     });
-    // context('as a User', function() {
-    //   it(`
-    //     a header with
-    //       no dropdown of login options.
-    //       an account dropdown containing
-    //         a link to my account settings.
-    //         a link to my communities.
-    //         a button to logout.
-    //   `, function() {
-    //     cy.signIn();
-    //     cy.visit('/');
-    //     cy.get('header button.account').click();
-    //     cy.get('??? a[href="/settings"]');
-    //     cy.get('??? a[href="/my-communities"]');
-    //     cy.get('??? button.logout');
-    //   });
-    // });
   });
+
+  // context('as a User', function() {
+  //   it(`
+  //     a header with
+  //       no dropdown of login options.
+  //       an account dropdown containing
+  //         a link to my account settings.
+  //         a link to my communities.
+  //         a button to logout.
+  //   `, function() {
+  //     cy.signIn();
+  //     cy.visit('/');
+  //     cy.get('header button.account').click();
+  //     cy.get('??? a[href="/settings"]');
+  //     cy.get('??? a[href="/my-communities"]');
+  //     cy.get('??? button.logout');
+  //   });
+  // });
 });
